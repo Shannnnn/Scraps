6fe28bcce78251e8b9e2739446ce984bfed8fa4e
Shannnnn <sacanoy16@gmail.com>
2020-01-18 02:33:55 +0800

    registration form

---
+++ app/components/RegistrationForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { 
+	StyleSheet, 
+	Text, 
+	View,
+	TextInput,
+	TouchableOpacity
+} from 'react-native';
+
+export default class RegistrationForm extends React.Component {
+	render() {
+		return (
+			<View style={styles.registrationform}>
+				<Text>Testing</Text>
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	registrationform : {
+
+	},
+})
